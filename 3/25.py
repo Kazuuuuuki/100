1c6ccb4564c2d4294ae829a5eae21e8b2d8db886
Kazuuuuuki <kazuki7932@gmail.co.jp>
2018-02-28 22:48:12 +0900

    done 25.py

---
+++ 3/25.py
@@ -0,0 +1,31 @@
+import func3 as f
+import re
+
+data = f.Engrand_data()
+lines = data.split("\n")
+p = re.compile('{{基礎情報')
+p2 = re.compile('}}')
+p3 = re.compile('=')
+p4 = re.compile('\*')
+values = {}
+#こうやって大きな値で設定するのは競プロとかしか使えない手かもしれない
+start_line = 100000
+for i in range(len(lines)):
+    if (p.search(lines[i]) and start_line == 100000):
+        start_line = i
+    if(i > start_line):
+        if(p3.search(lines[i])):
+            s = p3.search(lines[i])
+            start_id = s.start()
+            values[lines[i][1:start_id]] = lines[i][start_id+1:]
+            tmp = lines[i][1:start_id]
+        if(p4.match(lines[i])):
+            s = p4.search(lines[i])
+            end_id = s.end()
+            values[tmp] = values[tmp] + lines[i][end_id + 1:]
+
+    if (p2.match(lines[i]) and i > start_line):
+        break
+
+print(values)
+
