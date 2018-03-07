60f287ce4a8eaec3d56d0d83cfbd6dcca9b13151
Kazuuuuuki <kazuki7932@gmail.co.jp>
2018-03-07 23:45:44 +0900

    done 36

---
+++ 4/36.py
@@ -0,0 +1,16 @@
+import func4 as f
+values = {}
+data = f.morpheme()
+
+for i in range(len(data)):
+    for j in range(len(data[i])):
+        values[data[i][j]["surface"]] = 0
+
+
+for i in range(len(data)):
+    for j in range(len(data[i])):
+        values[data[i][j]["surface"]] += 1
+
+
+
+print(values)
